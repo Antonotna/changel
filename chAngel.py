310d553589e63139f993d94c1cf3ca624647c78a
unknown <moskalevab@6710l00k24A.msk.vtb24.ru>
2018-02-14 15:48:18 +0300

    - add copy to clipboard opportunity (pyperclip module) - fix tcl memory leak bug (CSCtf81249 caveats)

--- chAngel.py
+++ chAngel.py
@@ -1,6 +1,16 @@
 import sys
 import re
 
+try:
+	import pyperclip
+except ImportError:
+	clipFlag = False
+except:
+	print('Unexpected Import Error', sys.exc_info()[0])	
+	sys.exit()	
+else:
+	clipFlag = True
+
 class iface:
 	bw = 0
 	dl = 50000
@@ -14,7 +24,12 @@ def main():
 	ifList = []
 	ifMax = iface('None')
 	maxSum = 0
+	tclOut = []
 
+	if(clipFlag):
+		print('-'*40)
+		print('Result will be copied to clipboard')
+		print('-'*40)
 	print("Copy & Paste 'sh run | i interface Tunnel|delay|bandwidth' output chunk:")
 	for line in sys.stdin:
 		if(line == '\n'):
@@ -35,17 +50,25 @@ def main():
 			maxSum = i.Csum
 			ifMax = i
 
+	if(clipFlag):
+		for i in ifList: 
+			if(i == ifMax):
+				tclOut.append('if { [regexp "%s " [exec show ip interface brief] ] } { ios_config "interface %s" "no delay" "end" }' % (i.name, i.name) )
+			else:
+				tclOut.append('if { [regexp "%s " [exec show ip interface brief] ] } { ios_config "interface %s" "delay 1000000" "end" }' % (i.name, i.name) )
+		pyperclip.copy('tclsh\n\n' + ';'.join(tclOut) + ' ; tclquit\n')
 
-	print('---------Paste to CLI exec mode------------')
-	print('tclsh')
-	for i in ifList: 
-		if(i == ifMax):
-			print('if { [regexp "%s " [exec show ip interface brief] ] } { ios_config "interface %s" "no delay" }' % (i.name, i.name) )
-		else:
-			print('if { [regexp "%s " [exec show ip interface brief] ] } { ios_config "interface %s" "delay 1000000" }' % (i.name, i.name) )
-	print('exit')
-
-	input()
+	else:
+		print('---------Paste to CLI exec mode------------')
+		print('tclsh')
+		for i in ifList: 
+			if(i == ifMax):
+				print('\n\nif { [regexp "%s " [exec show ip interface brief] ] } { ios_config "interface %s" "no delay" "end" }\n' % (i.name, i.name) )
+			else:
+				print('\n\nif { [regexp "%s " [exec show ip interface brief] ] } { ios_config "interface %s" "delay 1000000" "end" }\n' % (i.name, i.name) )
+		print('tclquit')
+	
+		input()
 
 if(__name__ == '__main__'):
 	main()
